9ac72706b1da34e1e523a2be8ea6b803316da864
Morgan <morganlaflay@outlook.com>
2023-10-20 20:14:18 +0530

    add nav-component to index.js

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+// Expand-Collapse Animation
 document.querySelectorAll(".collapse").forEach((item) => {
   item.addEventListener("click", function () {
     this.classList.toggle("expandable");
   });
+});
+
+// NavBar Animation
+const nav = document.querySelectorAll(".nav__container")[0];
+const nav_bar = document.querySelectorAll(".collapse")[0];
+let currentScroll = nav.offsetHeight;
+window.addEventListener("scroll", () => {
+  if (window.scrollY > nav.offsetHeight) {
+    if (currentScroll <= scrollY) {
+      currentScroll = scrollY;
+      nav.classList.remove("sticky");
+      nav_bar.classList.remove("expandable");
+    } else {
+      nav.classList.add("sticky");
+      currentScroll = window.scrollY;
+      nav_bar.classList.remove("expandable");
+    }
+  } else {
+    nav.classList.remove("sticky");
+    nav_bar.classList.remove("expandable");
+  }
 });
